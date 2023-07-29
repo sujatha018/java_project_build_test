1600607fe3b6efe9bca09b77e4f929241f8c3d76
sujatha018 <sujamjrs018@gmail.com>
2023-07-29 13:40:29 +0530

    java-sample-project

---
+++ src/main/java/com/codeWithManju/EmployeeCrudAppApplication.java
@@ -0,0 +1,14 @@
+package com.codeWithManju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to CRUD operation");
+		SpringApplication.run(EmployeeCrudAppApplication.class, args);
+	}
+
+}
